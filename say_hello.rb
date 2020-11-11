595859df40e10a206998e3847ca7ec65d32c6b63
elisa <elisaopalka@gmail.com>
2020-11-11 14:21:20 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,8 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela"
-def say_hello(name = "Ruby programer")
-   puts "Hello, #{name}!"
+# I call on the method, say_hello, and give it the string "Gabriela" 
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
+say_hello("Gabriela")
+
